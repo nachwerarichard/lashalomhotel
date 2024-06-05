1c1db59893144eb86e1f10a89945953e78c1cb83
Nachwera Richard <87071510+nachwerarichard@users.noreply.github.com>
2024-06-05 15:15:43 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const navLinks = document.getElementById('navLinks');
 
     hamburger.addEventListener('click', () => {
         navLinks.classList.toggle('active');
+        hamburger.classList.toggle('active');
     });
 });
